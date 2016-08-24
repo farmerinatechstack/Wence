6a0af0695986dea2ca8559df17978c54d9adb8e0
Hassan Karaouni <hkII@Hassans-MBP-2.lan>
2016-08-23 18:29:45 -0700

    Caustic demo

---
+++ Assets/CausticProjection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CausticProjection : MonoBehaviour
+{
+	public float fps = 30.0f;
+	public Texture2D[] frames;
+	private int frameIndex;
+	private Projector projector;
+
+	void Start()
+	{
+		projector = gameObject.GetComponent<Projector> ();;
+		InvokeRepeating("NextFrame", 0, 1 / fps);
+	}
+
+	void NextFrame()
+	{
+		projector.material.SetTexture("_MainTex", frames[frameIndex]);
+		frameIndex = (frameIndex + 1) % frames.Length;
+	}
+}
