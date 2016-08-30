550d09c4ef2e66bbaacbba2bfc4f49febe10a558
Hassan Karaouni <hkII@Hassans-MacBook-Pro-2.local>
2016-08-30 00:52:26 -0700

    Ready to implement gaze features

--- Assets/PledgeDisplay.cs
+++ Assets/PledgeDisplay.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class PledgeDisplay : MonoBehaviour {
+	[SerializeField] Animator anim;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	// Is the pledge currently displayed?
+	private bool onDisplay = false;
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			TogglePledge ();
+		}
+	}
+
+	private void TogglePledge() {
+		if (!onDisplay) {
+			// Get pledge info
+			anim.Play ("Show");
+		} else {
+			anim.Play ("Hide");
+		}
+
+		onDisplay = !onDisplay;
 	}
 }
---
+++ Assets/TransformBob.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformBob : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		float yBob = Mathf.Sin (Time.realtimeSinceStartup) * 0.005f;
+		transform.position += new Vector3(0, yBob, 0);
+	}
+}
